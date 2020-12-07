6c8a678c1d0cf23a205302ab7128f005214ab391
Rúben Leonardo <rleonardo.pt@gmail.com>
2020-12-07 07:39:25 +0000

    Insert a Document

--- app.js
+++ app.js
@@ -16,6 +16,23 @@ client.connect((err) => {
     console.log("Connected successfully to server");
 
     const db = client.db(dbName);
-
-    client.close();
+    
+    insertDocuments(db, function() {
+        client.close();
+    });
 })
+
+const insertDocuments = function(db, callback) {
+    // Get the documents collection
+    const collection = db.collection('documents');
+    // Insert some documents
+    collection.insertMany([
+        {a: 1}, {a: 2}, {a: 3}
+    ], function(err, result) {
+        assert.equal(err, null);
+        assert.equal(3, result.result.n);
+        assert.equal(3, result.ops.length);
+        console.log("Inserted 3 documents into he collection");
+        callback(result);
+    });
+}
