3f81bc1411f8bb15e06ba262feb58ad19b9ea913
Rúben Leonardo <rleonardo.pt@gmail.com>
2020-12-07 11:43:26 +0000

    Update and remove a document. Change function to arrow keys

--- app.js
+++ app.js
@@ -12,23 +12,20 @@ const client = new MongoClient(url);
 
 // Use connect method to connect to the Server
 client.connect((err) => {
-    assert.equal(null, err);
+    assert.strictEqual(null, err);
     console.log("Connected successfully to server");
 
     const db = client.db(dbName);
     
-    insertDocuments(db, function() {
-        findDocuments(db, function() {
-            client.close();    
-        });
-
-        findDocumentsQueryFilter(db, function() {
-            client.close();
-        })
+    insertDocuments(db, () => {
+        findDocuments(db, () => client.close());
+        findDocumentsQueryFilter(db, () => client.close());
+        updateDocument(db, () => client.close());
+        removeDocument(db, ()=> client.close());
     });
 })
 
-const insertDocuments = function(db, callback) {
+const insertDocuments = (db, callback) => {
     // Get the documents collection
     const collection = db.collection('documents');
     // Insert some documents
@@ -43,14 +40,14 @@ const insertDocuments = function(db, callback) {
     });
 }
 
-const findDocuments = function(db, callback) {
+const findDocuments = (db, callback) => {
     // Get the documents collection
     const collection = db.collection('documents');    
     // The find() method selects documents in a collection or viw and returns a cursor
     // to the selected documents.
     // The toArray() method returns an array that contains all the documents
     // from a cursor.
-    collection.find({}).toArray(function(err, docs) {
+    collection.find({}).toArray((err, docs) => {
         assert.strictEqual(err, null);
         if (docs.length > 0) {
             console.log("Found the following records");
@@ -60,15 +57,40 @@ const findDocuments = function(db, callback) {
     });
 }
 
-const findDocumentsQueryFilter = function(db, callback) {
+const findDocumentsQueryFilter = (db, callback) => {
     // Get the documents collection
     const collection = db.collection('documents');
     // Find some documents
     // Only the documents which match 'a': 3 should be returned
     collection.find({'a': 3}).toArray((err, docs) => {
         assert.strictEqual(err, null);
-        console.log("Found the following recors");
+        console.log("Found the following records");
         console.log(docs);
         callback(docs);
+    });
+}
+
+const updateDocument = (db, callback) => {
+    // Get the documents collection
+    const collection = db.collection('documents');
+    // Update document where a is 2, set b equal to 1
+    collection.updateOne({ a: 2 },
+        { $set: { b: 1 }}, (err, result) => {
+            assert.strictEqual(err, null);
+            assert.strictEqual(1, result.result.n);
+            console.log("Updated the document with the field a equal to 2");
+            callback(result);
+        });
+}
+
+const removeDocument = (db, callback) => {
+    // Get the documents collection
+    const collection = db.collection('documents');
+    // Detele document where a is 3
+    collection.deleteOne({ a: 3}, (err, result) => {
+        assert.strictEqual(err, null);
+        assert.strictEqual(1, result.result.n);
+        console.log("Removed the document with the field a equal to 3");
+        callback(result);
     });
 }
